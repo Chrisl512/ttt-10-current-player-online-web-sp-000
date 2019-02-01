6c0167f1b8f7b31da2811984ef49cdc0ddc0db89
Christopher Luna <chris@illusionlayervfx.com>
2019-02-01 04:30:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,8 @@
 
 def turn_count(board)
   counter = 0
-  #board.each do |turn|
-  #  if turn == "X" || turn == "O"
+  board.each do |turn|
+    if turn == "X" || turn == "O"
     #counter += 1
  end
 end
